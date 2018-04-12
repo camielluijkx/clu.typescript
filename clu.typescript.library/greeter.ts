ac685e050c922ee8ce07dcb99637a62f8778984e
Camiel Luijkx <camiel.luijkx@gmail.com>
2018-04-12 22:28:23 +0200

    initial setup

---
+++ clu.typescript.library/greeter.ts
@@ -0,0 +1,19 @@
+﻿class Student {
+    fullName: string;
+    constructor(public firstName: string, public middleInitial: string, public lastName: string) {
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+}
+
+interface Person {
+    firstName: string;
+    lastName: string;
+}
+
+function greeter(person: Person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+
+let user = new Student("Camiel", "J.P..", "Luijkx");
+
+document.body.innerHTML = greeter(user);
